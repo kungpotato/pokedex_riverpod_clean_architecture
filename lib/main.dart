83a79f2a50b27b9eb76d766b2b9bca4b20a58ce4
Kittisak Pharaphan <kungpotato@users.noreply.github.com>
2024-05-15 07:32:52 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:pokedex/app/presentation/home/home_page.dart';
 import 'package:pokedex/core/providers/network_provider.dart';
 import 'package:pokedex/core/providers/storage_provider.dart';
+import 'package:pokedex/core/utils/test_asset_bundle.dart';
 import 'package:pokedex/observers.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -25,7 +26,10 @@ void main() async {
             .overrideWithValue(internetConnectionChecker),
         secureStorageProvider.overrideWithValue(flutterSecureStorage),
       ],
-      child: const MyApp(),
+      child: DefaultAssetBundle(
+        bundle: TestAssetBundle(),
+        child: const MyApp(),
+      ),
     ),
   );
 }
