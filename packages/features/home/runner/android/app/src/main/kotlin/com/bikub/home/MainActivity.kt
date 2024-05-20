2cf0d9f5f8b30be8e02556aef6de687abbce8fe0
Kittisak Pharaphan <kungpotato@users.noreply.github.com>
2024-05-20 21:17:03 +0700

    ok

---
+++ packages/features/home/runner/android/app/src/main/kotlin/com/bikub/home/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bikub.home
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- packages/features/home/runner/android/app/src/main/kotlin/com/bikub/home/dev/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.bikub.home.dev
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
