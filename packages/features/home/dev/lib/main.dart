17b1aaf9caf383d18e294ce0d4f98dd1d1a735f7
kungpotato <kittisak.py@gmail.com>
2024-05-20 17:48:32 +0700

    test

--- packages/features/home/dev/lib/main.dart
+++ packages/features/home/dev/lib/main.dart
@@ -1,10 +1,29 @@
 import 'package:auth/auth.dart';
+import 'package:core_providers/core_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:home/home.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final sharedPreferences = await SharedPreferences.getInstance();
+  final internetConnectionChecker = InternetConnectionChecker.createInstance();
+  const flutterSecureStorage = FlutterSecureStorage();
+
+  runApp(
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+        internetConnectionCheckerProvider
+            .overrideWithValue(internetConnectionChecker),
+        secureStorageProvider.overrideWithValue(flutterSecureStorage),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends ConsumerWidget {
@@ -19,7 +38,7 @@ class MyApp extends ConsumerWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: authState.when(data: (data) => const HomePage(), error: (error, stackTrace) => const Text('error'), loading: () => const CircularProgressIndicator(),),
+      home: authState.when(data: (data) => data.user!=null ?const HomePage():const LoginPage(), error: (error, stackTrace) => const Text('error'), loading: () => const CircularProgressIndicator(),),
     );
   }
 }
