1a7a510111005dd18991ba652f528dbfba033319
kungpotato <kittisak.py@gmail.com>
2024-05-20 13:02:02 +0700

    test

---
+++ packages/features/home/dev/android/app/src/main/kotlin/com/bikub/home/dev/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bikub.home.dev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ packages/features/home/dev/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:auth/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:home/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context,WidgetRef ref) {
+    final authState = ref.watch(authNotifierProvider);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: authState.when(data: (data) => const HomePage(), error: (error, stackTrace) => const Text('error'), loading: () => const CircularProgressIndicator(),),
+    );
+  }
+}
+
