ff44db7a178d7dd71c6e4b4dca3fcae9aa0f8854
Kittisak Pharaphan <kungpotato@users.noreply.github.com>
2024-05-30 14:48:38 +0700

    ok

---
+++ app/lib/gen_locale/app_home.dart
@@ -0,0 +1,6 @@
+import 'package:pokedex/localization/localize.dart';
+
+part 'app_home.g.dart';
+
+@Localize(name: 'Home', path: 'lib/localization/localization_config.json')
+class AppHome {}
--- app/lib/provider/localization_provider.dart
+++ app/lib/provider/localization_provider.dart
@@ -2,11 +2,8 @@ import 'dart:convert';
 
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pokedex/localization/localize.dart';
+import 'package:pokedex/gen_locale/app_home.dart';
 
-part 'localization_provider.g.dart';
-
-@Localize(name: 'Home', path: 'lib/localization/localization_config.json')
 final localizationProvider = Provider((ref) {
   final remoteConfig = FirebaseRemoteConfig.instance;
   final all = remoteConfig.getAll();
